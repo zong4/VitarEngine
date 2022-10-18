412c05f515887a2848a23a4f84a5760024cdc020
zong4 <2481814748@qq.com>
2022-10-18 13:50:37 +0800

    Add play and stop buttons.

--- VitarEditor/src/EditorLayer.h
+++ VitarEditor/src/EditorLayer.h
@@ -30,6 +30,12 @@ namespace Vitar
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
 
+		void OnScenePlay();
+		void OnSceneStop();
+
+		// UI Panels
+		void UI_Toolbar();
+
 	private:
 		Vitar::OrthographicCameraController m_CameraController;
 
@@ -59,8 +65,17 @@ namespace Vitar
 
 		int m_GizmoType = -1;
 
+		enum class SceneState
+		{
+			Edit = 0, Play = 1
+		};
+		SceneState m_SceneState = SceneState::Edit;
+
 		// Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 		ContentBrowserPanel m_ContentBrowserPanel;
+
+		// Editor resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 }
